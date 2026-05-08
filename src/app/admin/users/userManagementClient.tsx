e49741edb44e8286ae6f66c6420fcd2a79add6fa
Vladimir Dubecky <dubecky.vd@gmail.com>
2026-05-08 23:08:09 +0200

    feat(users-module): user edit and reset password logic

--- src/app/admin/users/userManagementClient.tsx
+++ src/app/admin/users/userManagementClient.tsx
@@ -5,28 +5,51 @@ import AppButton from "@/components/Common/AppButton";
 import Modal from "@/components/Overlay/Modal";
 import UsersTable from "@/components/Admin/UserTable";
 import CreateUserForm from "@/components/Forms/CreateUserForm";
+import {sendPasswordEmail} from "@/actions/admin/users";
+import {useToast} from "@/hooks/useToast";
 
 export default function UserManagementClient({ users, roles }) {
-    const [open, setOpen] = useState(false);
+    const toast = useToast();
 
-    return (
-        <>
+    const [open, setOpen] = useState(false);
+    const [editingUser, setEditingUser] = useState(null);
+
+    const onEdit = (user) => {
+        setEditingUser(user);
+        setOpen(true);
+    }
+
+    const onCreate = () => {
+        setEditingUser(null);
+        setOpen(true);
+    };
+
+    const onPasswordRestart = async (user) => {
+        const res = await sendPasswordEmail(user);
+        if(res.success) {
+            toast.success("Email pro restart hesla byl odeslán");
+        } else {
+            toast.error("Nepodařilo se odeslat email pro restart hesla");
+        }
+    }
+
+    return (<>
             <div className="flex justify-between mb-6">
                 <h1 className="text-2xl font-bold">
                     Správa uživatelů
                 </h1>
 
-                <AppButton onClick={() => setOpen(true)}>
+                <AppButton onClick={onCreate}>
                     Vytvořit uživatele
                 </AppButton>
             </div>
 
-            <UsersTable users={users}/>
+            <UsersTable users={users} onEdit={(user) => onEdit(user)} onPasswordRestart={(user) => onPasswordRestart(user)}/>
 
             {open && (
                 <Modal onClose={() => setOpen(false)}  contentClassName="max-w-lg w-full">
                     <div className="rounded-md border border-outline-variant bg-surface-container-low p-6 shadow-ambient">
-                        <CreateUserForm onSuccess={() => setOpen(false)} roles={roles}/>
+                        <CreateUserForm onSuccess={() => setOpen(false)} roles={roles} user={editingUser}/>
                     </div>
                 </Modal>
             )}
